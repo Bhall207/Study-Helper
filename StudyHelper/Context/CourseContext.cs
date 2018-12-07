b29e889189f994f76b5facc4f980700b9c98d5c9
Brittany Duke <bhall207@icloud.com>
2018-12-06 20:46:56 -0500

    Add project files.

---
+++ StudyHelper/Context/CourseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using StudyHelper.WebUI.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace StudyHelper.WebUI.Context
+{
+    public class CourseContext : DbContext
+    {
+       
+       public DbSet<Course> Courses { get; set; }
+    }
+
+    
+
+}
---
+++ StudyHelper/Context/CourseInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using StudyHelper.WebUI.Models;
+
+namespace StudyHelper.WebUI.Context
+{
+    public class CourseInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<CourseContext>
+    {
+        protected override void Seed(CourseContext context)
+        {
+            var Courses = new List<Course>
+            {
+                new Course {CourseID= 1017, CourseName="Biology", CourseDescription="Science cousre that explores the study of living organisms, divided into many specialized fields that cover their morphology, physiology, anatomy, behavior, origin, and distribution "  },
+                new Course {CourseID= 1738, CourseName="Literature", CourseDescription="opics include wide reading of historical and contemporary children's literature, criteria for selection, and practice in presenting and responding to literature ."},
+            };
+        }
+    }
+}
